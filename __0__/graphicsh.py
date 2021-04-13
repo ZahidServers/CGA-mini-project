16c574a714c10a2a44db205bbb79d5d40a2c68f5
ZahidServers <zahid.wadiwale1234@gmail.com>
2021-04-13 21:16:59 +0530

    Affan Zahid Milibhagat

--- __0__/graphicsh.py
+++
@@ -1,7 +0,0 @@
-import base64
-your_string="import pygame as graphics\nimport os\nimport random\nimport csv\nimport pickle\nimport button\nfrom pygame import mixer\nos.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = 'hide'"
-sample_string_bytes = "import sys\nimport os\nos.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = '1'\ndef blockPrint():\n\tsys.stdout = open(os.devnull, 'w')\nblockPrint()\nimport pygame as graphic\nimport button\nimport csv\nimport pickle".encode("ascii")
-base64_bytes = base64.b64encode(sample_string_bytes)
-your_string = base64_bytes.decode("ascii")
-with open("graphic.bin","wb") as f:
-    f.write(your_string.encode("utf-8"))
